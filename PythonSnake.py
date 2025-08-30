c19a1047a2a90681d623639303dcd0bf94bdafbf
SeviJack <167508118+SeviJack@users.noreply.github.com>
2025-08-30 12:36:00 -0700

    woooooo

--- PythonSnake.py
+++ PythonSnake.py
@@ -1,17 +1,40 @@
-import pyautogui
-import time
-import random
-import keyboard
-# import win32api
+import pyautogui, time, random, keyboard, ctypes, shutil, os
 from screeninfo import get_monitors
+
 
 # size of file step
 STEP = 20
 # user screen infos
-def set_up:
+
+# Game logic
+def play():
+    while True:
+        if keyboard.is_pressed("esc"):
+            break
+
+        if keyboard.is_pressed("w"):
+            pyautogui.moveTo
+        elif keyboard.is_pressed("a"):
+
+        elif keyboard.is_pressed("s"):
+
+        elif keyboard.is_pressed("d"):
+    
+    clean_up()
+
+
+def move_snake():
+    print("sep")
+
+# File logic
+def set_up():
+    desktop = os.path
+    # display files on the desktop
+    # switch folder? 
+    # switch back?
     for m in get_monitors(): 
-        
 
+def clean_up():
+    print("peace")
 
-
-while keyboard.is_pressed("esc") 
+play()  
