e9eb8e6c1fab73d5757db6ccbdd1c787a16aeb8f
SeviJack <167508118+SeviJack@users.noreply.github.com>
2025-08-30 11:56:32 -0700

    intialize

---
+++ PythonSnake.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time
+import random
+import keyboard
+# import win32api
+from screeninfo import get_monitors
+
+# size of file step
+STEP = 20
+# user screen infos
+def set_up:
+    for m in get_monitors(): 
+        
+
+
+
+while keyboard.is_pressed("esc") 
